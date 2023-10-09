be01765ebe49f59111db515fd9935674e81688fa
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-09 14:04:21 +0700

    first commit

---
+++ Days1.java
@@ -0,0 +1,13 @@
+
+package pkg100.days;
+public class Days {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("My name is Silfi kurnia");
+        System.out.println("class informatics B 23");
+        System.out.println("NIM D0223016");
+        System.out.println("Address Pasangkayu");
+    }
+    
+}
