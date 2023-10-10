5f4e804b36f2f8cd3476dd50b26e30c662ff6255
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-10 13:57:34 +0700

    day 2

---
+++ Day2.java
@@ -0,0 +1,19 @@
+
+package day2;
+
+public class Day2 {
+
+    public static void main(String[] args) {
+        String nama = "silfi kurnia ";
+        byte umur =18;
+        String namaayah = "syarifuddin ";
+        byte umurayah =49;
+        String namaibu = "syarifah ";
+        byte umuribu =39;
+        System.out.println(nama + umur);
+        System.out.println(namaayah + umurayah);
+        System.out.println(namaibu + umuribu);
+                
+    }
+    
+}
