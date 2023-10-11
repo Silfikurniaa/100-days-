ff95beea2e79834ebe4c71ea2433d597202e69d3
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-11 13:52:47 +0700

    day 3

---
+++ Day3.java
@@ -0,0 +1,13 @@
+
+package day3;
+public class Day3 {
+    public static void main(String[] args) {
+       int nilai = 2;
+       int angka = 3;
+       int jumlah;
+       jumlah = nilai + angka;
+        System.out.println("bilangan 1:" + nilai);
+        System.out.println("bilangan 2:" + angka);
+        System.out.println("jumlah=" +jumlah);}
+    
+}
