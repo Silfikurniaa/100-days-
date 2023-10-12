8310a2f14cdb735d2975641165048686136330ff
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-12 16:04:06 +0700

    day 4

---
+++ day4.java
@@ -0,0 +1,20 @@
+
+
+package javaapplication1;
+
+import java.util.Scanner;
+
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.println("masukkan gaji:");
+       double gaji=input.nextDouble ();
+        System.out.println("gaji" + gaji);
+        if (gaji<= 5000000) {
+        } else {
+        }
+        System.out.println("pajak 5%");
+        }
+    }
+
+
