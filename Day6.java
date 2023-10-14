78df28226a465058d361c0593ec886fc74e9fc08
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-14 11:50:48 +0700

    day 6

---
+++ Day6.java
@@ -0,0 +1,22 @@
+
+package day6;
+
+import java.util.Scanner;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+        System.out.println("angka pertama:");
+        int angka1=scanner.nextByte();
+        System.out.println("angka kedua");
+        int angka2=scanner.nextByte();
+                int jumlah= angka1+angka2 ;
+                 int kurang=angka1-angka2 ;
+                        System.out.println("hasil jumlah:"+ jumlah);
+                        System.out.println("hasil kurang:"+ kurang);
+                        
+                
+    }
+    
+}
