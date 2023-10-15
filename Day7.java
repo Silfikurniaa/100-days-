19a72d1710fce0d3e679df262b35f5ec1a803724
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-15 12:12:16 +0700

    day 7

---
+++ Day7.java
@@ -0,0 +1,16 @@
+
+package day7;
+public class Day7 {
+    public static void main(String[] args) {
+ int nilai = 5;
+ int nilai2 =8;
+ int nilai3 =10;
+ 
+        System.out.println("hasil:"); 
+        System.out.println(nilai2<nilai);
+        System.out.println(nilai*nilai2);
+        System.out.println(nilai3/nilai2);
+        System.out.println(nilai!=nilai2);
+    }
+    
+}
