c9ce984fde7d020c5e9d385e981b1cf5190f0f20
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-16 16:34:42 +0700

    tipedata string dan double

---
+++ Day8.java
@@ -0,0 +1,12 @@
+
+package day.pkg8;
+public class Day8 {
+
+    public static void main(String[] args) {
+String nama= "silfi";
+double gaji = 5000000.0;
+        System.out.println("nama " +nama);
+        System.out.println("gaji " +gaji);
+    }
+    
+}
