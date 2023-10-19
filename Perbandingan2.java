63b71b53232055e958c91a3a9a6c0b11d85a2f59
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-19 22:00:23 +0700

    perbandingan2

---
+++ Perbandingan2.java
@@ -0,0 +1,24 @@
+
+package perbandingan2;
+
+public class Perbandingan2 {
+
+    public static void main(String[] args) {
+int a=10;
+int b=5;
+boolean hasil;
+ hasil=(a==b);
+        System.out.println("a==b "+hasil);
+        hasil=(a!=b);
+        System.out.println("a!=b "+hasil);
+        hasil= (a>b);
+        System.out.println("a>b "+hasil);
+        hasil=(a<=b);
+        System.out.println("a<=b "+hasil);
+        hasil=(a>=b);
+        System.out.println("a>=b "+hasil);
+        
+       
+    }
+    
+}
