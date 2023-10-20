cccc62e6d21facd69234911e52dbfaf1a441e4d7
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-20 18:31:13 +0700

    umurdewasa

---
+++ UmurDewasa.java
@@ -0,0 +1,15 @@
+
+package umur.dewasa;
+
+public class UmurDewasa {
+
+    public static void main(String[] args) {
+int umur=20;
+if (umur<=18){
+    System.out.println("kamu sudah dewasa");
+}else {
+    System.out.println("Kamu masih remaja");
+}
+    }
+    
+}
