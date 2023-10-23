f1aabcc9d638315072a05c99df188fb42cdf4253
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-23 21:37:58 +0700

    seleksi

---
+++ Seleksi.java
@@ -0,0 +1,15 @@
+package seleksi;
+
+public class Seleksi {
+
+    public static void main(String[] args) {
+     float nilai =8.0f;
+     if(nilai>=7.5f)
+            System.out.println("anda di nyatakan lulus");
+     else
+            System.out.println("anda di nyatakan tidak lulus");
+     
+    }
+ 
+    
+}
