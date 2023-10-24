f977a4d9b9e1d3a0a570f8f9452fa8c0309c0f32
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-24 12:04:33 +0700

    Loopsementara

---
+++ Loopsementara.java
@@ -0,0 +1,14 @@
+
+package loopsementara;
+
+public class Loopsementara {
+
+    public static void main(String[] args) {
+
+int i=0;
+while (i<5) {
+    System.out.println(i);
+    i++;
+}}
+    
+}
