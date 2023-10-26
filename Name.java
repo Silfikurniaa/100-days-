78cb7090c7009b22f5b197fe339a9ad1cfd93241
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-26 20:37:08 +0700

    name

---
+++ Name.java
@@ -0,0 +1,22 @@
+
+package name;
+
+public class Name {
+
+    public static void main(String[] args) {
+String firstname= "silfi ";
+String lastname="kurnia";
+        System.out.println("nama "+firstname +lastname);
+byte umur=18;
+        System.out.println("umur "+umur);
+        float berat= (float) 45.6;
+        System.out.println("berat badan "+berat);
+        int tinggi= 154;
+        System.out.println("tinggi badan "+tinggi);
+        String status="mahasiswa";
+                System.out.println("status "+ status);
+                
+        
+        }
+    
+}
