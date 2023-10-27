3ecdc38836cf03ee9058ebec9ec209d29480f5f3
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-27 14:12:46 +0700

    VarContinue

---
+++ VarContinue.java
@@ -0,0 +1,17 @@
+
+package var.pkgcontinue;
+
+public class VarContinue {
+
+    public static void main(String[] args) {
+
+for (int i= 0; i<10; i++){
+if(i==5){
+    
+    continue;
+   
+}    
+    System.out.println(i);
+}}
+    
+}
