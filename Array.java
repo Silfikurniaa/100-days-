c1fe11abd3337d823d0df6a389a4fae63aa3d1e8
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-28 16:04:03 +0700

    array

---
+++ Array.java
@@ -0,0 +1,11 @@
+
+package array;
+public class Array {
+
+    public static void main(String[] args) {
+
+String[]Bunga={"bunga matahari", "bunga kertas", "bunga mawar", "bunga melati"   };
+        System.out.println(Bunga[2]);
+    }
+    
+}
