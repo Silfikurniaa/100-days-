b2e67b4a401f16fc828d5178b018950fc5d617ba
Silfikurniaa <vhiisilfi@gmail.com>
2023-10-30 20:19:29 +0700

    break

---
+++ Break.java
@@ -0,0 +1,21 @@
+
+package pkgbreak;
+
+public class Break {
+
+    public static void main(String[] args) {
+int i=0;
+while (i<15){
+    if(i==5){
+        i++;
+        continue;
+   
+    }
+    System.out.println(i);
+    i++;
+    }
+}
+}
+    
+    
+
