7e67c38734cdf43a0793133d1c53eca987f16ac5
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-03 21:13:02 +0700

    perulanganforeach

---
+++ Perulangan.java
@@ -0,0 +1,14 @@
+
+package perulangan;
+
+public class Perulangan {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++){
+            System.out.println("My Name Is Silfi");
+            
+        }
+  }
+    
+}
---
+++ Perulanganforeach.java
@@ -0,0 +1,11 @@
+package perulanganforeach;
+
+public class Perulanganforeach {
+
+    public static void main(String[] args) {
+int angka[]={3,1,42,24,15,20};
+for (int x:angka){
+    System.out.println(x+"");    
+}}
+    
+}
