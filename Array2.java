d203f1f615dfc3025f89861a6a8eb9f36ec5506c
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-05 20:55:11 +0700

    array2

---
+++ Array2.java
@@ -0,0 +1,11 @@
+
+package array2;
+
+public class Array2 {
+
+    public static void main(String[] args) {
+String []teman ={"siti","afifah", "eka", "yasmin", "wana"};
+        System.out.println(teman [1]);
+    }
+    
+}
