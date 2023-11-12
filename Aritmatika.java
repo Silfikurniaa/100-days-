e9cc533a1e66d1476dfb43549a7cd6c96077a390
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-12 20:14:35 +0700

    aritmatika

---
+++ Aritmatika.java
@@ -0,0 +1,19 @@
+
+package aritmatika;
+import java.util.Scanner;
+public class Aritmatika {
+
+    public static void main(String[] args) {
+int angka;
+int angka1;
+int hasil;
+Scanner sc = new Scanner(System.in);
+        System.out.println("Input angka-1: ");
+        angka =sc.nextInt();
+        System.out.println("Input angka-2: ");
+        angka1=sc.nextInt();
+        hasil= angka +angka1;
+        System.out.println("hasil= "+hasil);
+    }
+    
+}
