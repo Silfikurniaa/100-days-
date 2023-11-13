86b65479c8c987a7abe5429e745085f3e3652208
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-13 18:43:56 +0700

    aritmatikapengurangandanperkalian

---
+++ AritmatikaPengurangandanperkalian.java
@@ -0,0 +1,27 @@
+
+package aritmatikapengurangan;
+
+import java.util.Scanner;
+
+public class AritmatikaPengurangan {
+
+    public static void main(String[] args) {
+int angka;
+int angka1;
+int hasil;
+Scanner sc = new Scanner(System.in);
+        System.out.println("Input angka-1: ");
+        angka =sc.nextInt();
+        System.out.println("Input angka-2: ");
+        angka1=sc.nextInt();
+        hasil= angka -angka1;
+        System.out.println("hasil= "+hasil);
+         System.out.println("Input angka-1: ");
+        angka =sc.nextInt();
+        System.out.println("Input angka-2: ");
+        angka1=sc.nextInt();
+        hasil=angka*angka1;
+                System.out.println("hasil="+hasil);
+    }
+    
+}
