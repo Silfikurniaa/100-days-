8caff4f0af0534624835e94f96f158422f335aeb
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-16 19:19:40 +0700

    namabulan

---
+++ Namabulan.java
@@ -0,0 +1,14 @@
+
+package namabulan;
+
+public class Namabulan {
+
+    public static void main(String[] args) {
+String[] bulan={"Januari","Februari","Maret","April","Mei","juni","Juli", "Agustus", "September", "Oktober","November","Desember"};
+        System.out.println(" Nama-Nama Bulan");
+        for (int i = 0; i < bulan.length; i++) {
+            System.out.println("Bulan ke-" + (i+1)+ ":" +bulan[i]);
+            
+    }
+    
+}}
