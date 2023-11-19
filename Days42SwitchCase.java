e7c4d6feba544272497aaed98b4f384bc901acf8
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-19 20:49:11 +0700

    days42SwitchCase

---
+++ Days42SwitchCase.java
@@ -0,0 +1,22 @@
+
+package days42.pkgswitch.pkgcase;
+
+public class Days42SwitchCase {
+
+    
+    public static void main(String[] args) {
+        int pilihan =3;
+        switch (pilihan){
+            case 1:
+                case2:
+                System.out.println("pilihan 1 atau 2 terpenuhi.");
+                break;
+            case 3:
+                System.out.println("pilihan 3 terpenuhi.");
+                break;
+            default:
+                System.out.println("pilihan tidak terpenuhi");
+        }
+    }
+    
+}
