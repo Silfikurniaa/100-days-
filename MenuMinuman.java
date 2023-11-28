288c9cb9003d84a8a4222ca27bdbbcc08d539122
Silfikurniaa <vhiisilfi@gmail.com>
2023-11-28 21:23:49 +0700

    day 51

---
+++ MenuMinuman.java
@@ -0,0 +1,14 @@
+package menuminuman;
+
+public class MenuMinuman {
+
+    public static void main(String[] args) {
+        System.out.println("nama minuman: ");
+        String[] minuman = {"Red velvet", "Taro", "Cappucino", "Tiramisu"};
+        for (int i = 0; i < minuman.length; i++) {
+            System.out.println(minuman[i]);
+
+        }
+    }
+
+}
