8f6cf17d1f0cf1eac1376f9a03465153c01f1552
Silfikurniaa <vhiisilfi@gmail.com>
2023-12-05 23:26:04 +0700

    day 58

---
+++ Aarray2d.java
@@ -0,0 +1,21 @@
+
+package aarray2d;
+
+public class Aarray2d {
+
+    public static void main(String[] args) {
+int[][]nilai={
+    {24,31,68},
+        {55,83,99},
+        {45,77,23},};
+int jumlah=0;
+        for (int i = 0; i <nilai.length; i++) {
+            for (int j = 0; j <nilai[i].length; j++) {
+                jumlah+=nilai[i][j];
+                
+            }System.out.println("jumlah" + jumlah);
+            
+        }
+}
+    }
+    
