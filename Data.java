4e5ad4faa0dbfc7ecf1e7c8c7a319e8bc1421f6d
Silfikurniaa <vhiisilfi@gmail.com>
2023-12-08 22:20:46 +0700

    day 61

---
+++ Data.java
@@ -0,0 +1,30 @@
+
+package data;
+
+import java.util.Scanner;
+
+public class Data {
+
+    public static void main(String[] args){ ;
+             Scanner sc=new Scanner(System.in);
+             int [][]data= new int[2][3];
+             for (int i = 0; i <data.length; i++) {
+                 for (int j = 0; j <data[i].length; j++) {
+                     System.out.println("masukkan indeks array ke " +i+ ""+j+"");
+                     data[i][j]=sc.nextInt();
+                     
+                     
+                 }System.out.println();
+                 
+            
+        }
+             for (int i = 0; i <data.length; i++) {
+                 for (int j = 0; j <data[i].length; j++) {
+                     System.out.print(data [i][j]);
+                     
+                 }
+                 System.out.println();
+        }
+    }
+    
+}
