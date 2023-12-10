623adb2afff6d6055f8b713491f0995e5fe51daa
Silfikurniaa <vhiisilfi@gmail.com>
2023-12-10 21:20:46 +0700

    day 63

---
+++ Arrayy.java
@@ -0,0 +1,15 @@
+
+package arrayy;
+
+public class arrayy {
+
+    public static void main(String[] args) {
+int[]data={22,24,18,90};
+int i=0;
+do{
+    System.out.println(data[i]);
+    i++;
+}while(i<data.length);
+    }
+    
+}
