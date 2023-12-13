25a8fb10d84360d3a3705c8f522bfed45a80fe15
Silfikurniaa <vhiisilfi@gmail.com>
2023-12-13 22:26:31 +0700

    day 66

---
+++ Nilainegatif.java
@@ -0,0 +1,18 @@
+
+package nilainegatif;
+
+public class Nilainegatif {
+
+    public static void main(String[] args) {
+int []nilai={5,7,9,-2,-5,10,-4,8};
+int count=0;
+        System.out.println("nilai negatif dalam array : ");
+        for (int i = 0; i <nilai.length; i++) {
+            if(nilai[i]<0){
+                System.out.println(nilai[i]);
+            }
+            
+    }
+    
+    }
+}
