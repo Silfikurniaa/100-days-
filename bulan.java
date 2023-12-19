ce45e861ef2b8cf4b16fbb4dde22005374ac8d26
Silfikurniaa <vhiisilfi@gmail.com>
2023-12-19 22:50:58 +0700

    day 72

---
+++ bulan.java
@@ -0,0 +1,25 @@
+
+package pkg100.days;
+
+public class bulan {
+    public static void main(String[] args) {
+        int bulan=2;
+        switch(bulan){
+            case 1:
+                System.out.println("januari");
+                break;
+            case 2:
+                System.out.println("februari");
+                break;
+            case 3:
+                System.out.println("maret");
+                break;
+            case 4:
+                System.out.println("April");
+                break;
+            default:
+                System.out.println("nama bulan yang di inginkan tidak terdaftar");
+                
+        }
+    }
+}
