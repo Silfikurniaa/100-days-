17f534cb616ae15fc5ffa11b846f0f759c8ab0f2
Silfikurniaa <vhiisilfi@gmail.com>
2023-12-26 22:56:09 +0700

    day 79

---
+++ angkaganjill.java
@@ -0,0 +1,14 @@
+
+package pkg100.days;
+
+public class angkaganjill {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if(i%2!=0){
+                System.out.println(i+ " adalah angka ganjil ");
+        } else{
+                System.out.println(i+ " adalah angka genap");
+        }
+        }
+    }
+}
