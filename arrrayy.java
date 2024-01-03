d4d1e5dcbf6863c4aa8bf744ae3bc3b2a1248354
Silfikurniaa <vhiisilfi@gmail.com>
2024-01-03 21:21:48 +0700

    day 87

---
+++ arrrayy.java
@@ -0,0 +1,13 @@
+
+package pkg100.days;
+
+
+public class arrrayy {
+    public static void main(String[] args) {
+        double[]array={20.5, 44.0, 50.5,60.0, 90.5, 85.0};
+        for(double a: array){
+            System.out.println(a);
+        }
+                
+    }
+}
